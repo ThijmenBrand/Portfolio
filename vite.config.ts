e67e96ed48bb1f97589d8cef4c845d67f4eab647
Thijmen <Thijmen@ik.nu>
2023-05-27 22:37:57 +0200

    other config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vuetify({
+      autoImport: true,
+    }),
+  ],
+  define: { "process.env": {} },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
+  },
+  server: {
+    port: 3000,
+    open: true,
+    cors: true,
+  },
   base: "./",
 });
