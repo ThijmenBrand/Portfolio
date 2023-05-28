84585c7ca6fb263b1a020d21e691a55237ecc712
Thijmen <Thijmen@ik.nu>
2023-05-28 23:56:42 +0200

    contact form

--- src/views/components/contact.vue
+++ src/views/components/contact.vue
@@ -1,4 +1,6 @@
-<script setup lang='ts'></script>
+<script setup lang='ts'>
+import baseButton from './button.vue';
+</script>
 
 <template>
     <section>
@@ -6,22 +8,12 @@
         <div class="section-wrapper">
             <div class="contact-form">
                 <form action='' class='form'>
-                    <p class='field required half'>
-                        <label class='label required' for='name'>Name</label>
-                        <input class='text-input' id='name' name='name' required type='text'>
-                    </p>
-                    <p class='field required half'>
-                        <label class='label' for='email'>E-mail</label>
-                        <input class='text-input' id='email' name='email' required type='email'>
-                    </p>
-                    <p class='field'>
-                        <label class='label' for='message'>Message</label>
-                        <textarea class='textarea' cols='50' id='message' name='message' required rows='4'></textarea>
-                    </p>
-                    <p class='field'>
-                        <input class='button' type='submit' value='Send message'>
-                    </p>
+                    <input placeholder="Name" class='form-element' id='name' name='name' required type='text'>
+                    <input placeholder="Email" class='form-element' id='email' name='email' required type='email'>
+                    <textarea placeholder="Message" class='form-element' cols='50' id='message' name='message' required
+                        rows='4'></textarea>
                 </form>
+                <baseButton>Get in touch!</baseButton>
             </div>
             <div class="cv-section"></div>
         </div>
@@ -41,6 +33,34 @@ section {
         width: min(100ch, 100%);
         margin-inline: auto;
 
+        .contact-form {
+            form {
+                display: flex;
+                flex-direction: column;
+            }
+
+            .form-element {
+                background-color: transparent;
+                border-top: none;
+                border-left: none;
+                border-right: none;
+                border-bottom: 2px solid white;
+                margin: 2em;
+                transition: 0.5s;
+                color: white;
+
+                &::placeholder {
+                    color: white;
+                    font-family: 'Courier New', Courier, monospace;
+
+                }
+
+                &:focus {
+                    outline: none !important;
+                    border-bottom: 2px solid #6562cf;
+                }
+            }
+        }
 
     }
 }
